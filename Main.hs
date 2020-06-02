c8f2973678392683899f61640a8ddc33e25a3463
Pierre LERMUSIAUX <pierre.lermusiaux@loria.fr>
2020-06-02 17:48:30 +0200

    clean Main

--- Main.hs
+++ Main.hs
@@ -1,5 +1,3 @@
-{-# LANGUAGE OverloadedStrings #-}
-
 import Datatypes
 import FreeCheck
 import Parser
@@ -11,16 +9,3 @@ main = do
   case parseModule filename s of
     Left err -> putStrLn (show err)
     Right (Module sig trs) -> print (checkTRS sig trs)
-    --(getReachable sig (Appl "cons" [(Appl "list" [Var "l1"]) , (Var "l2")]) (TypeName "Expr"))
-
---import Data.Set as S
---import Data.Vector as V
---import Data.List
---
---main = do
---  let l1 = V.replicate 5 (S.singleton 5)
---  let l2 = [0..4]
---  let l = V.imap (foo l1) (V.fromList l2)
---  putStrLn (show l)
---    where foo li i n = accum add li [(i,n)]
---          add s n = S.insert n s
